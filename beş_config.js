ccb6491b56bdd62e352288fa63fd5bfd5a3cfe34
13ege23 <142740142+13ege23@users.noreply.github.com>
2023-08-28 16:06:48 +0300

    Add files via upload

---
+++ beş_config.js
@@ -0,0 +1,157 @@
+module.exports = {
+
+    token: "token..", 
+    guildID: "1140345885432221806", 
+    voiceChannelID: "1140345885897785482", 
+    presence: "kubinin ütopyasındaki bir bot", 
+    footer: "kubinin ütopyasındaki bir bot", 
+    botOwners: ["1091415573990219806"], 
+    prefix: ".",
+    manemoji: "♂️", 
+    womanemoji: "♀️", 
+    tagSymbol: "•", 
+    normalSymbol: "•", 
+    symbolBeş: "|", 
+    minageAge: 13,
+    welcomeResimURL: "https://cdn.discordapp.com/attachments/950167988127006821/1088263193832472656/beeautiful-sunset-illustration-1212023.png", 
+    welcomeResimRenk: "#861765",
+    kayitsizHesapIsim: "İsim | Yaş", 
+    supheliHesapIsim: "Şüpheli", 
+    banGif: "https://cdn.discordapp.com/attachments/1102681495283433612/1102723534612082798/wallpaperflare.com_wallpaper_1.jpg",
+    shipArkaplan: "https://cdn.discordapp.com/attachments/930486300639891466/1103290225750450196/pexels-eberhard-grossgasteiger-1421903.jpg",
+    selamVer:true, 
+
+
+    
+    mongoURL: "mongodb+srv://kub:123kubi456@cluster0.boibm5w.mongodb.net/?retryWrites=true&w=majority", 
+    topArkaplan: "https://cdn.discordapp.com/attachments/946826067174375494/1106207234783981678/pexels-eberhard-grossgasteiger-1421903.jpg", 
+    taskSystem: true, 
+
+    staffs: ["472100368227106866"], 
+
+    parents: { 
+        publicParents: ["1140345885897785482"],
+        registerParents: ["1140345885897785482"],
+        solvingParents: ["1140345885897785482"],
+        privateParents: ["1140345885897785482"],
+        aloneParents: ["1140345885897785482"],
+        funParents: ["1140345885897785482"]
+    },
+
+
+
+    limit: {
+        banLimit: 2,
+        jailLimit: 3,
+        muteLimit: 5,
+    },
+
+
+    logs: [
+        { name: "guard-log" },
+        { name: "family-log" },
+        { name: "command-log" },
+        { name: "ban-log" },
+        { name: "jail-log" },
+        { name: "vmute-log" },
+        { name: "mute-log" },
+        { name: "message-log" },
+        { name: "voice-log" },
+        { name: "register-log" },
+        { name: "şüpheli-log" },
+        { name: "rol-log" },
+        { name: "yasaklı-tag-log" },
+        { name: "rank-log" },
+        { name: "others-log" },
+        { name: "yetkili-başvurular" },
+        { name: "invite-log" }
+    ],
+    emojis: [
+        { name: "emote_hi", url: "https://cdn.discordapp.com/emojis/1102624348130521151.gif?size=128&quality=lossless" },
+        { name: "emote_true", url: "https://cdn.discordapp.com/emojis/1102619858941784155.webp?size=128&quality=lossless" },
+        { name: "emote_false", url: "https://cdn.discordapp.com/emojis/1102619855162703952.webp?size=128&quality=lossless" },
+        { name: "emote_warn", url: "https://cdn.discordapp.com/emojis/1102620367337562273.webp?size=128&quality=lossless" },
+        { name: "emote_man", url: "https://cdn.discordapp.com/emojis/1102620523780919376.webp?size=128&quality=lossless" },
+        { name: "emote_woman", url: "https://cdn.discordapp.com/emojis/1102620489429565553.webp?size=128&quality=lossless" },
+        { name: "emote_chat", url: "https://cdn.discordapp.com/emojis/1102621542363439305.webp?size=128&quality=lossless" },
+        { name: "emote_computer", url: "https://cdn.discordapp.com/emojis/1102621545022627850.webp?size=128&quality=lossless" },
+        { name: "emote_invite", url: "https://cdn.discordapp.com/emojis/1102621541121925262.webp?size=128&quality=lossless" },
+        { name: "emote_voice", url: "https://cdn.discordapp.com/emojis/1102621543709806602.webp?size=128&quality=lossless" },
+        { name: "emote_coin", url: "https://cdn.discordapp.com/emojis/1102622139376480276.gif?size=128&quality=lossless" },
+        { name: "emote_home", url: "https://cdn.discordapp.com/emojis/1102622711718612993.webp?size=128&quality=lossless" },
+        { name: "emote_value", url: "https://cdn.discordapp.com/emojis/1102622713408929923.gif?size=128&quality=lossless" },
+        { name: "emote_others", url: "https://cdn.discordapp.com/emojis/1102624779950895134.webp?size=128&quality=lossless" },
+        { name: "emote_emptystart", url: "https://cdn.discordapp.com/emojis/1102623376083791882.webp?size=128&quality=lossless" },
+        { name: "emote_empty", url: "https://cdn.discordapp.com/emojis/1102623377421766687.webp?size=128&quality=lossless" },
+        { name: "emote_emptyend", url: "https://cdn.discordapp.com/emojis/1102623378919149668.webp?size=128&quality=lossless" },
+        { name: "emote_fillstart", url: "https://cdn.discordapp.com/emojis/1102623397550227577.webp?size=128&quality=lossless" },
+        { name: "emote_fill", url: "https://cdn.discordapp.com/emojis/1102623398913376347.webp?size=128&quality=lossless" },
+        { name: "emote_fillend", url: "https://cdn.discordapp.com/emojis/1102623401337696349.webp?size=128&quality=lossless" },
+        { name: "emote_cmute", url: "https://cdn.discordapp.com/emojis/1102741501890547794.webp?size=128&quality=lossless" },
+        { name: "emote_vmute", url: "https://cdn.discordapp.com/emojis/1102741503329194035.webp?size=128&quality=lossless" },
+        { name: "emote_zero", url: "https://cdn.discordapp.com/emojis/1103844496162160650.gif?size=128&quality=lossless" },
+        { name: "emote_one", url: "https://cdn.discordapp.com/emojis/1103844498670354522.gif?size=128&quality=lossless" },
+        { name: "emote_two", url: "https://cdn.discordapp.com/emojis/1103844441598464040.gif?size=128&quality=lossless" },
+        { name: "emote_three", url: "https://cdn.discordapp.com/emojis/1103844446468063282.gif?size=128&quality=lossless" },
+        { name: "emote_four", url: "https://cdn.discordapp.com/emojis/1103844448787505232.gif?size=128&quality=lossless" },
+        { name: "emote_five", url: "https://cdn.discordapp.com/emojis/1103844452218458212.gif?size=128&quality=lossless" },
+        { name: "emote_six", url: "https://cdn.discordapp.com/emojis/1103844319644885082.gif?size=128&quality=lossless" },
+        { name: "emote_seven", url: "https://cdn.discordapp.com/emojis/1103844321591038104.gif?size=128&quality=lossless" },
+        { name: "emote_eight", url: "https://cdn.discordapp.com/emojis/1103844324346699796.gif?size=128&quality=lossless" },
+        { name: "emote_nine", url: "https://cdn.discordapp.com/emojis/1103844326095732776.gif?size=128&quality=lossless" },
+        { name: "emote_submessage", url: "https://cdn.discordapp.com/emojis/1103846079235108924.webp?size=128&quality=lossless" },
+        { name: "emote_web", url: "https://cdn.discordapp.com/emojis/1105493458933792819.webp?size=128&quality=lossless" },
+        { name: "emote_camera", url: "https://cdn.discordapp.com/emojis/1105493457453187162.webp?size=128&quality=lossless" },
+        { name: "emote_time", url: "https://cdn.discordapp.com/emojis/1105494402136285376.webp?size=128&quality=lossless" }
+    ],
+
+
+    burcRoles: [
+        { name: "Koç", icon: "https://cdn.discordapp.com/emojis/1054803690848006164.png?size=128&quality=lossless" },
+        { name: "Boğa", icon: "https://cdn.discordapp.com/emojis/1054803741250957324.png?size=128&quality=lossless" },
+        { name: "İkizler", icon: "https://cdn.discordapp.com/emojis/1054803754232324107.png?size=128&quality=lossless" },
+        { name: "Yengeç", icon: "https://cdn.discordapp.com/emojis/1054788879422599209.png?size=128&quality=lossless" },
+        { name: "Aslan", icon: "https://cdn.discordapp.com/emojis/1054803703808397413.png?size=128&quality=lossless" },
+        { name: "Başak", icon: "https://cdn.discordapp.com/emojis/1054803729901166622.png?size=128&quality=lossless" },
+        { name: "Terazi", icon: "https://cdn.discordapp.com/emojis/1099539509596663819.png?size=128&quality=lossless" },
+        { name: "Akrep", icon: "https://cdn.discordapp.com/emojis/1054803768031588403.png?size=128&quality=lossless" },
+        { name: "Yay", icon: "https://cdn.discordapp.com/emojis/1054788894262038548.png?size=128&quality=lossless" },
+        { name: "Oğlak", icon: "https://cdn.discordapp.com/emojis/1054803665367617586.png?size=128&quality=lossless" },
+        { name: "Kova", icon: "https://cdn.discordapp.com/emojis/1054803678168629398.png?size=128&quality=lossless" },
+        { name: "Balık", icon: "https://cdn.discordapp.com/emojis/1054803716689113108.png?size=128&quality=lossless" },
+    ],
+
+    iliskiRoles: [
+        { name: "Sevgilim Var", icon: "💖", color: "#ff0000" },
+        { name: "Sevgilim Yok", icon: "🙄", color: "#2e5a6e" },
+        { name: "Sevgili Yapmıyorum", icon: "🥱", color: "#bf9f3d" },
+    ],
+
+    etkinlikRoles: [
+        { name: "Çekiliş Katılımcısı", icon: "🎉", color: "#21a327" },
+        { name: "Etkinlik Katılımcısı", icon: "📣", color: "#2151a3" },
+    ],
+
+    renkRoles: [
+        { name: "🍓", color: "#ff0000" },
+        { name: "🍏", color: "#00ff00" },
+        { name: "🍌", color: "#fffb17" },
+        { name: "🍇", color: "#7417ff" },
+        { name: "🌼", color: "#ffffff" },
+        { name: "🥕", color: "#ff8400" },
+    ],
+
+    levelRoles: [
+        { name: "Chat Bronze", color: "#b47402" },
+        { name: "Chat Silver", color: "#b1b1b1" },
+        { name: "Chat Gold", color: "#faff00" },
+        { name: "Chat Diamond", color: "#00fffb" },
+        { name: "Voice Bronze", color: "#b47402" },
+        { name: "Voice Silver", color: "#b1b1b1" },
+        { name: "Voice Gold", color: "#faff00" },
+        { name: "Voice Diamond", color: "#00fffb" },
+    ]
+
+
+
+}
